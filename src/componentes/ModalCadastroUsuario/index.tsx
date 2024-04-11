0625e2f397604c96c438a8fc89f4b0981bcac11f
willjunger <williamjunger@hotmail.com>
2024-04-10 23:47:30 -0300

    adiciona campos do formulario e seues estados

--- src/componentes/ModalCadastroUsuario/index.tsx
+++ src/componentes/ModalCadastroUsuario/index.tsx
@@ -1,12 +1,36 @@
-import { AbModal } from "ds-alurabooks"
+import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks"
 
 import imagemPrincipal from './assets/login.png';
+import { useState } from "react";
 
 const ModalCadastroUsuario = () => {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [complemento, setComplemento] = useState('');
+    const [cep, setCep] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senhaConfirmada, setSenhaConfirmada] = useState('');
+
     return (<AbModal titulo="Cadastrar" aberta={true} aoFechar={() => console.log('fecha ai')}>
         <figure>
             <img src={imagemPrincipal} alt="Monitor com uma fechadura e uma pessoa com uma chave ao lado." />
         </figure>
+
+        <form>
+            <AbCampoTexto label='Nome' value={nome} onChange={setNome} />
+            <AbCampoTexto label='Email' value={email} onChange={setEmail} />
+            <AbCampoTexto label='Endereco' value={endereco} onChange={setEndereco} />
+            <AbCampoTexto label='Complemento' value={complemento} onChange={setComplemento} />
+            <AbCampoTexto label='Cep' value={cep} onChange={setCep} />
+            <AbCampoTexto label='Senha' value={senha} onChange={setSenha} />
+            <AbCampoTexto label='SenhaConfirmada' value={senhaConfirmada} onChange={setSenhaConfirmada} />
+
+            <footer>
+                <AbBotao texto="Cadastrar" />
+            </footer>
+        </form>
+
     </AbModal>)
 }
 

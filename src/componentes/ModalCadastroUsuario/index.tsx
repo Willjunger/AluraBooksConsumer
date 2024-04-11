ee88deedfa8c1257023adbbeb86becc21f8e2183
willjunger <williamjunger@hotmail.com>
2024-04-10 22:22:06 -0300

    adiciona imagem ao modal

--- src/componentes/ModalCadastroUsuario/index.tsx
+++ src/componentes/ModalCadastroUsuario/index.tsx
@@ -1,8 +1,12 @@
 import { AbModal } from "ds-alurabooks"
 
+import imagemPrincipal from './assets/login.png';
+
 const ModalCadastroUsuario = () => {
     return (<AbModal titulo="Cadastrar" aberta={true} aoFechar={() => console.log('fecha ai')}>
-        <h1>Olá Mundo</h1>
+        <figure>
+            <img src={imagemPrincipal} alt="Monitor com uma fechadura e uma pessoa com uma chave ao lado." />
+        </figure>
     </AbModal>)
 }
 

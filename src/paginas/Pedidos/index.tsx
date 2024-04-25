b9985c43c5a015a7809c7d07f74a72dc01667feb
willjunger <williamjunger@hotmail.com>
2024-04-25 19:59:55 -0300

    adiciona logica para remover pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -22,6 +22,18 @@ const Pedidos = () => {
             .catch(erro => console.log(erro))
     }, [])
 
+    const excluir = (pedido: IPedido) => {
+        const token = sessionStorage.getItem('token')
+        axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(() => {
+            setPedidos(pedidos.filter(p => p.id !== pedido.id))
+        })
+            .catch(erro => console.log(erro))
+    }
+
 
     return (<section className="pedidos">
         <h1>Meus Pedidos</h1>
@@ -32,6 +44,9 @@ const Pedidos = () => {
                     <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
                     <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
                     <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+                    <li>
+                        <button onClick={() => excluir(pedido)}>Excluir</button>
+                    </li>
                 </ul>
                 <AbBotao texto="Detalhes" />
             </div>

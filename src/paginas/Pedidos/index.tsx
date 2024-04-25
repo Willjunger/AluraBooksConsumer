a35b527fb85e01c344108ab2763e355e186f58ab
willjunger <williamjunger@hotmail.com>
2024-04-25 19:52:47 -0300

    adicionando requisicao e envio de token para salvar estado dos dados dos pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,19 +1,40 @@
 import { AbBotao } from "ds-alurabooks";
 
+import axios from 'axios'
+
 import './Pedidos.css'
+import { useEffect, useState } from "react";
+import { IPedido } from '../../interfaces/IPedido'
 
 const Pedidos = () => {
+
+    const [pedidos, setPedidos] = useState<IPedido[]>([])
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+
+        axios.get<IPedido[]>('http://localhost:8000/pedidos', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(resposta => setPedidos(resposta.data))
+            .catch(erro => console.log(erro))
+    }, [])
+
+
     return (<section className="pedidos">
         <h1>Meus Pedidos</h1>
-        <div className="pedido">
-            <ul>
-                <li>Pedido: <strong>123456</strong></li>
-                <li>Data do pedido: <strong>12/08/1234</strong></li>
-                <li>Valor total: <strong>R$ 45.00</strong></li>
-                <li>Entrega realizada em: <strong>13/09/2134</strong></li>
-            </ul>
-            <AbBotao texto="Detalhes" />
-        </div>
+        {pedidos.map(pedido =>
+            <div className="pedido" key={pedido.id}>
+                <ul>
+                    <li>Pedido: <strong>{pedido.id}</strong></li>
+                    <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+                    <li>Valor total: <strong>R$ {pedido.total}</strong></li>
+                    <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+                </ul>
+                <AbBotao texto="Detalhes" />
+            </div>
+        )}
     </section>)
 }
 
